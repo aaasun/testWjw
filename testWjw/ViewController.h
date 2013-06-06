5ab7c8fc77fbedda083391396eb056f517d6c763
front <front@fronttekiiMac.local>
2013-06-06 18:04:03 +0800

    Revert "ViewController"
    This reverts commit db28c565ce57e74af45e2d4c0bb356ee7921ee18.

--- testWjw/ViewController.h
+++ testWjw/ViewController.h
@@ -6,34 +6,8 @@
 //  Copyright (c) 2013年 front. All rights reserved.
 //
 
-
-#import "blockView.h"
-#import "ibView.h"
-
-#import "linkedTextView.h"
-
-#import <QuartzCore/QuartzCore.h>
-#import "timeToString.h"
-#import "timeStampClass.h"
-
-
 #import <UIKit/UIKit.h>
-#import <dispatch/dispatch.h>
 
-@interface ViewController : UIViewController<linkedTextViewDelegate,UISearchDisplayDelegate>
-{
-    ibView *viewTest;
-    
-    NSMutableArray *arrData;
-    
-//    CAScrollLayer
-    
-    UISearchDisplayController *searchView;
-    
-}
-
-@property (retain, nonatomic) IBOutlet UISearchBar *mySearchBar;
-@property (retain, nonatomic) IBOutlet UIImageView *selfBigImg;
-@property (retain, nonatomic) IBOutlet UIActivityIndicatorView *indicatView;
+@interface ViewController : UIViewController
 
 @end
